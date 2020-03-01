dde44f91e91e3add07eac597fc6e3db38bee5fb8
Sébastien Combéfis <seb478@gmail.com>
2020-03-01 16:55:07 +0100

    Clean up code
    - Removed unnecessary code
    - Fixed spelling

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <b-alert v-if="data.status == 'success'" show variant="info">{{data.output}}</b-alert>
-        <b-alert v-else-if="data.status == 'unreachable'" show variant="danger">Chould not reach pythia</b-alert>
+        <b-alert v-else-if="data.status == 'unreachable'" show variant="danger">Could not reach Pythia.</b-alert>
         <b-alert v-else show variant="danger">Error: {{data.status}}</b-alert>
     </div>
 </template>
